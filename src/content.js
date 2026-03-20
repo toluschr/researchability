6ea03d107bb8cef391f1704481a028294015cacd
toluschr <toluschr@protonmail.com>
2026-03-20 09:11:34 +0100

    Fix code blocks

--- src/content.js
+++ src/content.js
@@ -2,7 +2,18 @@ import { Readability } from "@mozilla/readability";
 import TurndownService from 'turndown';
 
 let article = new Readability(document.cloneNode(true)).parse();
-let turndownService = new TurndownService();
+let turndownService = new TurndownService({codeBlockStyle: "fenced"});
+
+turndownService.addRule('preCodeBlock', {
+    filter: 'pre',
+    replacement: function (content, node, options) {
+        let code = node.textContent;
+        let language = node.dataset.language ?? "";
+
+        return `\n\`\`\`${language}\n${code}\`\`\`\n`;
+    }
+});
+
 let markdown = turndownService.turndown(article.content);
 let blob = new Blob([markdown], { type: "text/markdown" });
 let link = document.createElement("a");
